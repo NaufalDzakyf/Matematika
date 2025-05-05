743bf49cdb75fc70210049da8441a581ffda9d66
NaufalDzakyf <104565321+NaufalDzakyf@users.noreply.github.com>
2025-05-05 22:29:42 +0700

    Add files via upload

---
+++ quiz.php
@@ -0,0 +1,153 @@
+<?php
+session_start();
+include('connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>teachers</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<header class="header">
+   
+   <section class="flex">
+
+      <a href="beranda.php" class="logo">Matematika</a>
+
+      <form action="search.php" method="post" class="search-form">
+         <input type="text" name="search_box" required placeholder="search courses..." maxlength="100">
+         <button type="submit" class="fas fa-search"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <img src="images/pic-1.jpg" class="image" alt="">
+         <h3 class="name">shaikh anas</h3>
+         <p class="role">studen</p>
+         <a href="profile.php" class="btn">view profile</a>
+         <div class="flex-btn">
+            <a href="logout.php" class="option-btn">Logout</a>
+         </div>
+      </div>
+
+   </section>
+
+</header>   
+
+<div class="side-bar" data-scrolled="true">
+
+   <div id="close-btn">
+      <i class="fas fa-times"></i>
+   </div>
+
+   <div class="profile">
+      <img src="images/pic-1.jpg" class="image" alt="">
+      <h3 class="name">shaikh anas</h3>
+      <p class="role">studen</p>
+      <a href="profile.php" class="btn">view profile</a>
+   </div>
+
+   <nav class="navbar">
+      <a href="beranda.php"><i class="fas fa-home"></i><span>Beranda</span></a>
+      <a href="video.php"><i class="	fa fa-video-camera"></i><span>Video</span></a>
+      <a href="materi.php"><i class="fa fa-book"></i><span>Materi</span></a>
+      <a href="quiz.php"><i class="fa fa-pencil"></i><span>Quiz</span></a>
+   </nav>
+
+</div>
+
+<section class="teachers">
+
+   <h1 class="heading">Quiz</h1>
+
+   <form action="" method="post" class="search-tutor">
+      <input type="text" name="search_box" placeholder="search tutors..." required maxlength="100">
+      <button type="submit" class="fas fa-search" name="search_tutor"></button>
+   </form>
+
+   <div class="box-container">
+      <div class="box">
+         <div class="tutor">
+            <i class="fa fa-pie-chart"></i>
+            <div>
+               <h3>Statistika</h3>
+            </div>
+         </div>
+         <p>total playlists : <span>4</span></p>
+         <p>total videos : <span>18</span></p>
+         <p>total likes : <span>1208</span></p>
+         <a href="teacher_profile.php" class="inline-btn">view</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <i class="fa-solid fa-dice"></i>
+            <div>
+               <h3>Peluang</h3>
+            </div>
+         </div>
+         <p>total playlists : <span>4</span></p>
+         <p>total videos : <span>18</span></p>
+         <p>total likes : <span>1208</span></p>
+         <a href="teacher_profile.php" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+         <i class="fa-solid fa-grip">‌</i>
+            <div>
+               <h3>Matriks</h3>
+            </div>
+         </div>
+         <p>total playlists : <span>4</span></p>
+         <p>total videos : <span>18</span></p>
+         <p>total likes : <span>1208</span></p>
+         <a href="teacher_profile.php" class="inline-btn">View </a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="footer">
+
+   &copy; copyright @ 2022 by <span>mr. web designer</span> | all rights reserved!
+
+</footer>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+   
+</body>
+</html>
